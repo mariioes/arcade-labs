a6a03a036359f53d8fc79653939f0577012bb036
Mario Esteban Simón <mario.estebans@edu.uah.es>
2026-02-05 20:49:36 +0100

    Change greeting text from 'Hello, world' to 'Buenos dias'

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -6,7 +6,7 @@ HEIGHT = 600
 arcade.open_window(WIDTH, HEIGHT, "Example")
 
 arcade.start_render()
-arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
+arcade.draw_text("Buenos dias", 350, 300, arcade.color.WHITE)
 arcade.finish_render()
 
 arcade.run()
